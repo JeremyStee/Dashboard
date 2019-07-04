8b5d4944d43e037f615e8f4c8a99f0fda489cdc4
Jérémy Steenkiste <jeremy.steenkiste.pro@gmail.com>
2019-07-04 22:28:25 +0200

    Mise en place de l'architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MainContainerComponent } from './main-container/main-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainContainerComponent
   ],
   imports: [
     BrowserModule,
